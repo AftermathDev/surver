8bb7b77f72c78f64ca7272e22aa8b1d6946d9314
rullinoiz <>
2021-05-14 00:31:52 +0000

    more stuff

--- server.py
+++ server.py
@@ -2,17 +2,59 @@
 
 import sys
 import numpy
-import socket
-import selectors
-import argparse
+import socketio
 
-parser = argparse.ArgumentParser()
+from _thread import start_new_thread
 
-fallback = {         # in case the user did not supply any arguments
-  ip = "127.0.0.1",
-  port = 25454
+
+# initalize socket
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+# in case the user did not supply any arguments
+fallback = {
+  "ip": socket.gethostbyname(''),
+  "port": 25454
 }
 
-parser.add_argument("-ip", type=str)
-parser.add_argument("-port","-p", type=int)
+try:
+  s.bind(fallback.ip, fallback.port)
+except socket.error as e:
+  print("Did not bind successfully: " + str(e))
 
+s.listen(30)
+print("Waiting for connection")
+
+currentId = 0
+pos = {}
+def threaded_client(conn):
+    global currentId, pos
+    conn.send(str.encode(currentId))
+    reply = ''
+    while True:
+        try:
+            data = conn.recv(2048)
+            reply = data.decode('utf-8')
+            if not data:
+                conn.send(str.encode("Goodbye"))
+                break
+            else:
+                print("Recieved: " + reply)
+                arr = reply.split(":")
+                id = int(arr[0])
+                pos[id] = reply
+
+                reply = pos[nid][:]
+                print("Sending: " + reply)
+
+            conn.sendall(str.encode(reply))
+        except:
+            break
+
+    print("Connection Closed")
+    conn.close()
+
+while True:
+    conn, addr = s.accept()
+    print("Connected to: ", addr)
+
+    start_new_thread(threaded_client, (conn,))
